1cfde0c2d60b84ca9724ba97719742f38bc96a3d
Mariam Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2023-10-16 19:58:34 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -97,13 +97,13 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1,
-        customerName: 'Morich Brown',
+        customerName: 'Anastasia Dan',
         rating: 4.5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
     {
         imgURL: customer2,
-        customerName: 'Lota Mongeskar',
+        customerName: 'Anastasia Dan',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
