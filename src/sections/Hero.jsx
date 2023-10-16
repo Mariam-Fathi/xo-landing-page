affd0428f9945459f4d5d438164d4bb7cf2cca7c
Mariam Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2023-10-16 19:46:45 +0300

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           Our Summer collections
         </p>
 
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[68px] max-sm:leading-[82px] font-bold'>
             NEW ARRIVALS
           <br />
           <span className='text-coral-red inline-block mt-3'>XO</span> Shoes
